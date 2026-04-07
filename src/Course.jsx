32c2899f7bbf3179f62056d61998a48aaad2d9b6
Aadhishmugunthan <aadhishmugunthan2002@gmail.com>
2026-04-07 23:53:35 +0530

    updated

--- src/Course.jsx
+++ src/Course.jsx
@@ -1,28 +1,40 @@
 import html from './assets/hero.png'
 import PropTypes from 'prop-types'
+import { useState } from 'react'
+
 function Course(props) {
 
-    function BuyCourse(discount,e){
-        console.log(props.name,"purchased",discount,"% discount");
+    const [purchased, setPurchased] = useState(false)
+
+    function BuyCourse(discount, e) {
+        console.log(props.name, "purchased", discount, "% discount");
+        setPurchased(true)
         console.log(e);
     }
 
-    
     return (
         props.name &&
         <div className="card">
-            <img src={props.image} alt={props.name} />
-            <h3>{props.name}</h3>
-            <p>{props.prize}</p>
-            <button onClick={()=>BuyCourse (20)}>Buy Now</button>
 
-            
+            <img src={props.image} alt={props.name} />
+
+            <h3>{props.name}</h3>
+
+            <p>{props.prize}</p>
+
+            <button
+                onClick={(event) => BuyCourse(20, event)}
+            >
+                Buy Now
+            </button>
+
+            <p>
+                {purchased ? "Already purchased" : "Get it now"}
+            </p>
+
         </div>
     );
-
-
 }
-
 
 /*Course.defaultProps = {
     name: "Aadhish Course",
@@ -30,11 +42,13 @@ function Course(props) {
     image: html
 }*/
 
-Course.propTypes ={
-    name:PropTypes.string,
-    rating : PropTypes.number,
-    show : PropTypes.bool
 
+Course.propTypes = {
+    name: PropTypes.string,
+    rating: PropTypes.number,
+    show: PropTypes.bool
 }
 
 export default Course;
+
+
